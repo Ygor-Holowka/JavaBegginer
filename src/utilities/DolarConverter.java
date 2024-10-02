4c0c27aafc19a69769be994451782c7cb2e39024
holowka <holowka@192.168.5.122>
2024-10-01 23:14:59 -0300

    First Commit

---
+++ src/utilities/DolarConverter.java
@@ -0,0 +1,7 @@
+package utilities;
+
+public class DolarConverter {
+    public static double converter(double dolar, double qtdDollar){
+        return (dolar * qtdDollar) + (dolar * qtdDollar * 0.06);
+    }
+}
