f404cd1e7027a0dbc42dc6f7361c1c412b25efe7
YGOR HOLOWKA <68624664+Ygor-Holowka@users.noreply.github.com>
2024-10-11 16:14:23 -0300

    Delete src directory

--- src/utilities/DolarConverter.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class DolarConverter {
-    public static double converter(double dolar, double qtdDollar){
-        return (dolar * qtdDollar) + (dolar * qtdDollar * 0.06);
-    }
-}
